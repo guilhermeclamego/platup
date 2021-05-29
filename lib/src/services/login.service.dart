6b7ca5ee170e06f807abdc760a0b3a9ff3761a6c
guilherme.lamego <guilherme.lamego@betha.com.br>
2021-05-29 01:31:04 -0300

    Ajuste login service

--- lib/src/services/login.service.dart
+++ lib/src/services/login.service.dart
@@ -2,13 +2,12 @@ import 'package:http/http.dart' as http;
 import 'package:platup/src/shared/models/usuario.model.dart' as usuarioModel;
 
 void loginService(usuario, senha) async {
-  var jsonUsuario =
-      new usuarioModel.Usuario(usuario, senha).toJson().toString();
+  var jsonUsuario = new usuarioModel.Usuario(usuario, senha).toJson();
   print(jsonUsuario);
   var headers = {'Content-Type': 'application/json'};
   var request =
       http.Request('POST', Uri.parse('https://3e76e4fc3f1b.ngrok.io/login'));
-  request.body = jsonUsuario;
+  request.body = jsonUsuario.toString();
   request.headers.addAll(headers);
 
   http.StreamedResponse response = await request.send();
