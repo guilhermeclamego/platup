27f26d5b88b1a40f615fd1c72ee2a305dc0b7ec1
guilherme.lamego <guilherme.lamego@betha.com.br>
2021-05-29 01:50:36 -0300

    Limpeza de codigo

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,10 @@ import 'package:platup/src/components/login.page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  static const String _title = 'PlatUp';
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: LoginPage(),
     );
